f2cf03a666c558f302d3f14120f51c2197838c91
Ralf Gabriels <ralfg@hotmail.be>
2017-03-17 18:39:44 +0100

    Modified correlations output
    - Added output_name variable (avoid "Filename.extSuffix.ext" filenames)
    - Added correlations output to csv
    - Added options for correlations output (boxplots or csv)

---
+++ run_cluster.sh
@@ -0,0 +1,3 @@
+python ms2pipC.py -c config_cluster.file -s data/Cluster_Phospho.mgf data/Cluster_Phospho.csv
+#python ms2pipC.py -c config_cluster.file -s data/Cluster_ModNonPhospho.mgf data/Cluster_ModNonPhospho.csv
+#python ms2pipC.py -c config_cluster.file -s data/Cluster_NonMod.mgf data/Cluster_NonMod.csv
